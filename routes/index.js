56914e752e71f59fdf52d3f2bb1db7e41a778084
shashank-singh2017 <shashank.singh@sjsu.edu>
2018-11-01 07:20:10 -0700

    modified the arpau query

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const csvdata = require('csvdata');
 var mysql = require('mysql');
 
 var conn  = mysql.createConnection({
-    host            : 'mysql://35.232.12.175:3306/',
+    host            : '35.232.12.175:3306',
     user            : 'root',
     password        : 'root',
     database        : 'challenge'
